a1ff443410456d5c35d1c71b06b01962325662b9
Leonardo Rafael de Castro <lrccastro4@gmail.com>
2026-03-11 16:13:42 -0300

    avanços na aula

--- BancosSQLite.py
+++ BancosSQLite.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 #conexao = sqlite3.connect(':memory:') Cria um banco temporaria na Ram, quando o programa encerra apaga tudo. Útil pra testes rápidos.
 
 # Conexao abre a porta para o banco de dados
@@ -18,10 +19,39 @@ cursor = conexao.cursor()
 #      );
 #  """)
 
-cursor.execute('''DELETE FROM cliente WHERE id = 2''')
+# varios_inserts = [
+#     ('Apolo', 'Apolo@gmail.com', '45685698'),
+#     ('Eric', 'Eric@gmail.com', '12345678'),
+#     ('Paulo', 'Paulo@gmail.com', '81579723')
+# ]
 
-cursor.execute("""SELECT * FROM cliente""")
-print(cursor.fetchall())
+# O executemany executa vários scripts de uma vez
+# cursor.executemany("INSERT INTO cliente (nome, email, senha) VALUES (?, ?, ?)", varios_inserts)
 
+# rowid mostra o id da linha, mas eu já havia criado a propriedade id
+#cursor.execute("""SELECT rowid, * FROM cliente""")
+
+cursor.execute("""SELECT * FROM cliente WHERE id = "" """)
+
+tabela = cursor.fetchall()
+
+for item in tabela:
+     print(item)
+
+#print(cursor.fetchall())
+
+# many para vários
+#print(cursor.fetchmany(2))
+
+# one para apenas um a partr de um id. Para pegar uma linha específica é necessário usar WHERE
+#print(cursor.fetchone())
+
+# Imprime os itens sparando por linha formatando
+# tabela = cursor.fetchall()
+#
+# for item in tabela:
+#     print(item[1] + "\t\t" + item[2])
+
+# Para enviar as mudanças(insert, update, delete, etc) utiliza-se o commit, igual em um código no git hub
 conexao.commit()
 conexao.close()
